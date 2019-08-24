833e8acc85edca878e3dd72f47844f428d21ea23
AkulG1 <32958511+AkulG1@users.noreply.github.com>
2019-08-24 14:01:59 +0530

    Basic Design

---
+++ config/passport.js
@@ -0,0 +1,77 @@
+var LocalStrategy   = require('passport-local').Strategy;
+
+var User            = require('../app/models/user');
+
+module.exports = function(passport) {
+    passport.use('local-login', new LocalStrategy({
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our form
+
+        User.findOne({ 'local.email' :  email }, function(err, user) {
+
+            if (err)
+                return done(err);
+
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // 
+
+            if (!user.validPassword(password))
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+            return done(null, user);
+        });
+
+    }));
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    passport.use('local-signup', new LocalStrategy({
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) {
+
+        // asynchronous
+        // User.findOne wont fire unless data is sent back
+        process.nextTick(function() {
+
+        User.findOne({ 'local.email' :  email }, function(err, user) {
+            // if there are any errors, return the error
+            if (err)
+                return done(err);
+
+            // check to see if theres already a user with that email
+            if (user) {
+                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+            } else {
+                var newUser = new User();
+
+                newUser.local.email    = email;
+                newUser.local.password = newUser.generateHash(password);
+
+                newUser.save(function(err) {
+                    if (err)
+                        throw err;
+                    return done(null, newUser);
+                });
+            }
+
+        });    
+
+        });
+
+    }));
+
+};
